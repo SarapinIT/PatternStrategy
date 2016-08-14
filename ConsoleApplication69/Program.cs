1d7e2910055ceca313f2ad3bdacd8c115930ecee
sarapinit <sarapinit@ya.ru>
2016-08-14 18:50:12 +0700

    Add project files.

---
+++ ConsoleApplication69/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace StrategyPoly
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var context = new Context(new ConcreteStrategy1());
+      context.DoWork();
+      context.Strategy = new ConcreteStrategy2();
+      context.DoWork();
+
+      Console.ReadKey();
+    }
+  }
+
+  public class Context
+  {
+    public Context(IStrategy strategy)
+    {
+      if (strategy == null) throw new ArgumentException(nameof(strategy));
+
+      Strategy = strategy;
+    }
+    private IStrategy _strategy;
+
+    public IStrategy Strategy
+    {
+      get { return _strategy; }
+      set
+      {
+        if (value == null) throw new ArgumentException(nameof(value));
+        _strategy = value;
+      }
+    }
+    public void DoWork()
+    {
+      Strategy.DoWork();
+    }
+  }
+
+  public interface IStrategy
+  {
+    void DoWork();
+  }
+
+  public class ConcreteStrategy1 : IStrategy
+  {
+    public void DoWork()
+    {
+      Console.WriteLine("strategy work 1");
+    }
+  }
+
+  public class ConcreteStrategy2 : IStrategy
+  {
+    public void DoWork()
+    {
+      Console.WriteLine("strategy work 2");
+    }
+  }
+}
---
+++ StrategyDele/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StrategyDele
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var context = new Context(() => Console.WriteLine("strategy work 1"));
+      context.DoWork();
+      context.Strategy = () => Console.WriteLine("strategy work 1");
+      context.DoWork();
+
+      Console.ReadKey();
+    }
+  }
+
+  public class Context
+  {
+    public Context(Action strategy)
+    {
+      if (strategy == null) throw new ArgumentException(nameof(strategy));
+
+      Strategy = strategy;
+    }
+    private Action _strategy;
+
+    public Action Strategy
+    {
+      get { return _strategy; }
+      set
+      {
+        if (value == null) throw new ArgumentException(nameof(value));
+        _strategy = value;
+      }
+    }
+    public void DoWork()
+    {
+      Strategy();
+    }
+  }
+}
